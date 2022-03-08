773e0b31deb5fdcd941a316b33b0010df092e280
Aishwarya <aishu.parama@gmail.com>
2022-03-08 19:22:26 +0530

    I practice some java script basic concept

---
+++ aishwarya.js
@@ -0,0 +1,19 @@
+console.log("Hello world!")
+var name="Aishwarya";
+console.log(name);
+var name="Siddharth";
+console.log(name);
+var age="34,10"
+console.log(age);
+//var x="x";
+//var y="y";
+//console.log("x+y");
+var num1=10;
+var num2=30;
+console.log(num1+num2);
+var country="India";
+console.log("We are from "+country);
+var num=100;
+(num++);
+
+console.log(num++);
